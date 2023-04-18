27466ad0ad68811bfc31c0080256935ce3f97fb2
Nikolay <50394337+puz27@users.noreply.github.com>
2023-04-18 23:32:31 +0300

    Update queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,13 +1,13 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-    select customers.company_name, employees.first_name ,employees.last_name, shippers.company_name  from orders
+    SELECT customers.company_name,customers.city, employees.first_name ,employees.last_name, employees.city,shippers.company_name  FROM orders
     INNER JOIN customers
-    ON orders.customer_id = customers.customer_id
+    ON orders.customer_id = customers.customer_id AND customers.city = 'London'
     INNER JOIN employees
-    ON orders.employee_id =employees.employee_id
+    ON orders.employee_id = employees.employee_id AND employees.city = 'London'
     INNER JOIN shippers
-    ON orders.ship_via =shippers.shipper_id
+    ON orders.ship_via = shippers.shipper_id AND shippers.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
