5f3af65c6bfc84b2177e25065ab843137def4897
anv <anv@woori.ru>
2023-04-18 11:21:38 +0300

    add cursor all

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,44 +1,51 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 import psycopg2
+import csv
 
 
-# my_connection = psycopg2.connect(
-#     host="localhost",
-#     database="test",
-#     user="postgres",
-#     password="y00ddaev"
-# )
-#
-# cursor = my_connection.cursor()
-# #cursor.execute("INSERT INTO town (id, size) VALUES(12, 1000)")
-# #cursor.execute("INSERT INTO town (id, size) VALUES(333, 1000)")
-# #cursor.executemany("INSERT INTO town (id, size) VALUES (%s, %s)", [(20, "Данные"), (19, "Другие")])
-# my_connection.commit()
-#
-#
-# cursor.execute("select * from town")
-# rows = cursor.fetchall()
-# print(rows)
-# cursor.close()
-# my_connection.close()
+def get_data_csv(path):
+    data = []
+    try:
+        with open(path) as csv_file:
+            take_csv_data = csv.reader(csv_file, delimiter=",")
+            count = 0
+            for roe in take_csv_data:
+                if count != 0:
+                    data.append(roe)
+                count += 1
+    except FileNotFoundError:
+        print("File not found!")
+    return data
 
-connector = psycopg2.connect(
-host="localhost",
-database="test",
-user="postgres",
-password="y00ddaev"
-)
 
-try:
-    with connector:
-        with connector.cursor() as cursor:
-            cursor.execute("INSERT INTO town (id, size) VALUES(30, 1000)")
-            cursor.execute("select * from town")
-            get = cursor.fetchall()
-            print(get)
-finally:
-    connector.close()
+customers_data = get_data_csv("north_data/customers_data.csv")
+orders_data = get_data_csv("north_data/orders_data.csv")
+employees_data = get_data_csv("north_data/employees_data.csv")
 
+
+def send_query(table, args):
+    connection = psycopg2.connect(
+        host="localhost",
+        database="north",
+        user="postgres",
+        password="y00ddaev"
+    )
+
+    try:
+        with connection.cursor() as cursor:
+            col_count = "".join("%s," * len(args[0]))
+            query = f"INSERT INTO {table} VALUES ({col_count[:-1]})"
+            cursor.executemany(query, args)
+            connection.commit()
+    # except psycopg2.Error:
+    #     print("Ошибка с запросом.")
+    finally:
+        connection.close()
+
+
+send_query("customers", customers_data)
+send_query("orders", orders_data)
+send_query("employees", employees_data)
 
 
 
