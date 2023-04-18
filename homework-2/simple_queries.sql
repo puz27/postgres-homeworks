718134fe96f20299ddef9b1ee327c964c90e0c29
Nikolay <50394337+puz27@users.noreply.github.com>
2023-04-18 13:03:32 +0300

    Update simple_queries.sql

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -3,7 +3,7 @@
 select contact_name, country from customers
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-
+select order_id, ABS(order_date - shipped_date) AS Forming  from orders;
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
 
