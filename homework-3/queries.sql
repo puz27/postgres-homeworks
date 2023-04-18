098c7ddae907152c114c53432d3bde2a437cb875
Nikolay <50394337+puz27@users.noreply.github.com>
2023-04-18 23:58:33 +0300

    add 2 answer

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -13,7 +13,12 @@
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+    SELECT products.product_name, products.units_in_stock, suppliers.contact_name ,suppliers.phone FROM products
+    INNER JOIN suppliers
+    ON products.supplier_id = suppliers.supplier_id AND discontinued = 0 AND units_in_stock < 25
+    INNER JOIN categories
+    ON products.category_id = categories.category_id AND category_name IN ('Dairy Products','Condiments')
+    ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
