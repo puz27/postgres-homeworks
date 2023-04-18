9aa505013cd003c18058c09c577f3c0a596e704e
anv <anv@woori.ru>
2023-04-18 11:23:19 +0300

    add sql query

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,3 +1,20 @@
--- SQL-команды для создания таблиц
-CREATE TABLE customers (customer_id VARCHAR(20) PRIMARY KEY, company_name VARCHAR(50), contact_name VARCHAR(30));
+CREATE TABLE customers (
+	customer_id VARCHAR(20) PRIMARY KEY, 
+	company_name VARCHAR(50) NOT NULL, 
+	contact_name VARCHAR(30));
 
+
+CREATE TABLE orders (
+	order_id INT PRIMARY KEY, 
+	customer_id VARCHAR(30) REFERENCES CUSTOMERS(customer_id) not NULL , 
+	employee_id INT NOT NULL, 
+	order_date DATE NOT NULL, 
+	ship_city VARCHAR(30) NOT NULL);
+	
+	
+CREATE TABLE employees (
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	title VARCHAR(50) NOT NULL,
+	birth_date DATE NOT NULL,
+	notes TEXT);
