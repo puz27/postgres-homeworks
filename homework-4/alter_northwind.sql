34841c89b55b4925a7a5819dabe35714606cc8b8
Nikolay <50394337+puz27@users.noreply.github.com>
2023-04-20 14:32:04 +0300

    add 3

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -8,7 +8,11 @@ ALTER TABLE products
 ADD CHECK (discontinued IN (0, 1))
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
-
+CREATE TABLE new_table_products 
+AS (SELECT * 
+	FROM products 
+	WHERE discontinued = 1
+   )
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
